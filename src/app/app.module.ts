6884fe94dd9ee86e11a2302eb62d4f30d1bdb904
Matthew Davis <matthew@matthewdavis.io>
2018-11-06 03:11:45 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,69 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CarouselCardComponent } from './carousel-card/carousel-card.component';
+import { SearchComponent } from './search/search.component';
+import { TrackListComponent } from './track-list/track-list.component';
+import { ThermometerComponent } from './thermometer/thermometer.component';
+import { HeaderComponent } from './header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+    declarations: [
+
+        AppComponent,
+        AboutComponent,
+        CarouselComponent,
+        CarouselCardComponent,
+        SearchComponent,
+        TrackListComponent,
+        ThermometerComponent,
+        HeaderComponent,
+        HomeComponent
+
+    ],
+    imports: [
+
+        BrowserModule,
+        NgbModule,
+
+        RouterModule.forRoot([
+
+            {
+
+                path: 'home',
+                component: HomeComponent
+
+            }, {
+
+                path: 'about',
+                component: AboutComponent
+
+            }, {
+
+                path: 'search',
+                component: SearchComponent
+
+            }, {
+
+                path: '',
+                pathMatch: 'full',
+                redirectTo: 'home'
+
+            }
+
+        ])
+
+    ],
+
+    providers: [],
+    bootstrap: [AppComponent]
+
 })
-export class AppModule { }
+export class AppModule {
+}
